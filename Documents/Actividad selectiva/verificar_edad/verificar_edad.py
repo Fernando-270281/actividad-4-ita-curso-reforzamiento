8a02fcdba6d0d5069e442a411ba89525b3d98312
Fernando <felixflod27@gmail.com>
2026-07-27 18:10:19 -0600

    Agregar programa de verificacion de edad

---
+++ Documents/Actividad selectiva/verificar_edad/verificar_edad.py
@@ -0,0 +1,10 @@
+# Verificar edad para votar
+# Pedimos la edad al usuario
+edad = int(input("¿Cual es tu edad? "))
+
+# Comprobamos si es mayor o igual a 18
+if edad >= 18:
+    print("Si puede votar")
+else:
+    print("No puede votar")
+        
